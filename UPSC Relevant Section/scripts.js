b724337266f024d1243c7d1a7a9399c9bc3085d0
Abhishek Biradar <programmeratrax@gmail.com>
2025-12-03 14:40:48 +0530

    Update scripts.js

--- UPSC Relevant Section/scripts.js
+++ UPSC Relevant Section/scripts.js
@@ -312,7 +312,7 @@
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="transition: transform 0.2s"><path d="m6 9 6 6 6-6"/></svg>
                                     </button>
                                     
-                                    <p style="font-size:small; color:#e0e3e1"><i>This model answer is just a suggestion of how to write. WE PROMOTE WRITING THE ANSWERS ON YOUR OWN SO THAT YOU GRADUALLY GAIN PERFECTION IN EXPRESSING YOUR THOUGHTS ON PAPER, which is certainly necessary for the UPSC Examinations.</i></p>
+                                    <p style="font-size:small; color:grey"><i>This model answer is just a suggestion of how to write. WE PROMOTE WRITING THE ANSWERS ON YOUR OWN SO THAT YOU GRADUALLY GAIN PERFECTION IN EXPRESSING YOUR THOUGHTS ON PAPER, which is certainly necessary for the UPSC Examinations.</i></p>
                                     
                                     <!-- HIDDEN ANSWER CONTAINER -->
                                     <div class="model-answer-container">
@@ -372,6 +372,7 @@
             document.addEventListener('keydown', (e) => { if (e.key === 'Escape') closeModalDirect(); });
 
         });
+
 
 
 
